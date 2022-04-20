ab9b6d1ac02b98fb002bc1ed107f2ef9445d2ed9
Kiss Máté Botond <k.matebotond@gmail.com>
2022-04-21 00:07:46 +0200

    Update site

--- conways_game_of_life/conways_game_of_life.js
+++ conways_game_of_life/conways_game_of_life.js
@@ -1,14 +1,14 @@
-var canvas = document.getElementById("conways_game_of_life")
-var ctx = canvas.getContext("2d")
-var width = canvas.width
-var height = canvas.height
-var rows = 25
-var unit = width / rows
+let canvas = document.getElementById("conways_game_of_life")
+let ctx = canvas.getContext("2d")
+let width = canvas.width
+let height = canvas.height
+let rows = 25
+let unit = width / rows
 
-var cells = array2d(rows, rows, false)
+let cells = array2d(rows, rows, false)
 drawCells()
 
-var gameIntervalID = -1
+let gameIntervalID = -1
 function start() {
   if (gameIntervalID == -1) {
   gameIntervalID = setInterval(game, 200)
@@ -37,14 +37,14 @@ function toggleCell(event) {
 function game() {
   let cells2 = array2d(rows, rows, false)
   for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < rows; j++) {
-  let neighbors = countNeighbors(i, j)
-  if (!cells[i][j] && neighbors == 3) {
-  cells2[i][j] = true
-  } else if (cells[i][j]) {
-  cells2[i][j] = neighbors == 2 || neighbors == 3
-  }
-  }
+    for (let j = 0; j < rows; j++) {
+      let neighbors = countNeighbors(i, j)
+      if (!cells[i][j] && neighbors == 3) {
+        cells2[i][j] = true
+      } else if (cells[i][j]) {
+        cells2[i][j] = neighbors == 2 || neighbors == 3
+      }
+    }
   }
   cells = cells2
   drawCells()
@@ -55,33 +55,33 @@ function drawCells() {
   ctx.fillRect(0, 0, width, height)
   ctx.strokeStyle = "white"
   ctx.fillStyle = "yellow"
-  for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < rows; j++) {
-  ctx.beginPath()
-  ctx.rect(i * unit, j * unit, unit, unit)
-  if (cells[i][j]) {
-  ctx.fill()
-  } else {
-  ctx.stroke()
-  }
-  }
+    for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < rows; j++) {
+      ctx.beginPath()
+      ctx.rect(i * unit, j * unit, unit, unit)
+      if (cells[i][j]) {
+        ctx.fill()
+      } else {
+        ctx.stroke()
+      }
+    }
   }
 }
 
 function countNeighbors(x, y) {
   let count = 0
   for (let i = -1; i <= 1; i++) {
-  for (let j = -1; j <= 1; j++) {
-  if (!(i == 0 && j == 0)) {
-  let neighborX = (x + i) % 25
-  neighborX = neighborX == -1 ? (rows - 1) : neighborX
-  let neighborY = (y + j) % 25
-  neighborY = neighborY == -1 ? (rows - 1) : neighborY
-  if (cells[neighborX] && cells[neighborX][neighborY]) {
-  count++
-  }
-  }
-  }
+    for (let j = -1; j <= 1; j++) {
+      if (!(i == 0 && j == 0)) {
+        let neighborX = (x + i) % 25
+        neighborX = neighborX == -1 ? (rows - 1) : neighborX
+        let neighborY = (y + j) % 25
+        neighborY = neighborY == -1 ? (rows - 1) : neighborY
+        if (cells[neighborX] && cells[neighborX][neighborY]) {
+          count++
+        }
+      }
+    }
   }
   return count
 }
@@ -89,10 +89,10 @@ function countNeighbors(x, y) {
 function array2d(a, b, fill) {
   let array = new Array(a)
   for (let i = 0; i < a; i++) {
-  array[i] = new Array(b)
-  for (let j = 0; j < b; j++) {
-  array[i][j] = fill
-  }
+    array[i] = new Array(b)
+    for (let j = 0; j < b; j++) {
+      array[i][j] = fill
+    }
   }
   return array
 }
